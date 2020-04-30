2986282f40f75f7d909563ca1b990e79abd8f40c
Murlin Wei <murlin@wei.com>
2020-04-29 20:29:13 -0400

    New command

--- src/index.js
+++ src/index.js
@@ -1,38 +1,52 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const https = require('https');
+const https = require("https");
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === '!NickPlz') {
-    
-    process.on('uncaughtException', (error) => {
-      msg.channel.send("<@117809686543204361> your server errored with:\n" + JSON.stringify(error));
+client.on("message", (msg) => {
+  if (msg.content === "!NickPlz") {
+    process.on("uncaughtException", (error) => {
+      msg.channel.send(
+        "<@117809686543204361> your server errored with:\n" +
+          JSON.stringify(error)
+      );
     });
 
-    https.get('https://nick.denaro.dev/routes', (resp) => {
-      let data = '';
-      resp.on('data', (chunk) => {
+    https.get("https://nick.denaro.dev/routes", (resp) => {
+      let data = "";
+      resp.on("data", (chunk) => {
         data += chunk;
       });
 
-      resp.on('end', () => {
-        if (!data.includes('swagger-ui')) {
-          msg.channel.send("<@117809686543204361>, an annonucement: plz https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger?view=aspnetcore-3.1#swagger-ui");
+      resp.on("end", () => {
+        if (!data.includes("swagger-ui")) {
+          msg.channel.send(
+            "<@117809686543204361>, an annonucement: plz https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger?view=aspnetcore-3.1#swagger-ui"
+          );
         } else {
-          msg.channel.send("<@118534418125029379> <@!118536732500164610> IT'S GO TIME");
+          msg.channel.send(
+            "<@118534418125029379> <@!118536732500164610> IT'S GO TIME"
+          );
         }
       });
     });
-  } else if(msg.content === '!DrewPlz') {
-    if(msg.member.voice.channel.members.map(user => user.displayName).includes('Drew')) {
-      msg.channel.send("<@118534418125029379> <@!118536732500164610> <@117809686543204361> an annonucement: IT'S GO TIME");
+  } else if (msg.content === "!DrewPlz") {
+    if (
+      msg.member.voice.channel.members
+        .map((user) => user.displayName)
+        .includes("Drew")
+    ) {
+      msg.channel.send(
+        "<@118534418125029379> <@!118536732500164610> <@117809686543204361> an annonucement: IT'S GO TIME"
+      );
     } else {
       msg.channel.send("<@118533809598627843> IT'S THURSDAY");
     }
+  } else if(msg.content === "!Hey") {
+    msg.channel.send("Hey :)")
   }
 });
 
